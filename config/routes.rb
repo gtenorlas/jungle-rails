58e62f67b8c2f3a8a2de66f0957caf0105cbae18
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-24 02:55:14 +0000

    Add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,15 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  #Create new users
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 

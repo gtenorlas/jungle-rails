33f621b448ab1edb6394966b7c9743d157711469
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-24 02:33:27 +0000

    Add routes to /signup and /users

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

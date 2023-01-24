b54f24b10f24ebed8820a6400be139e0dd8c6a2b
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-24 01:26:27 +0000

    Implement create/new method in Admin/Categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,4 +4,26 @@ class Admin::CategoriesController < ApplicationController
    def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category successfully created!'
+    else
+      render :new
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+  private :category_params
 end

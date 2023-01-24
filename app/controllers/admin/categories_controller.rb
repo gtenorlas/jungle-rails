d9182f8f5ad50e201641e0c34fe955781db59869
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-24 00:49:30 +0000

    Implement method Admin/Categories index

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
-  def index
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
+
+   def index
+    @categories = Category.all
   end
-end

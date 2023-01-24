36bb6c7803de2313bbd57842f379144d404ba230
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-24 04:02:15 +0000

    Refactor User model to add validation and self authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: {minimum: 8, message: "Password must be at least 8 characters" } 
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.strip.downcase
+    user = User.find_by_email(email)
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end

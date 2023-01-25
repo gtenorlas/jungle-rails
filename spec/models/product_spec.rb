b731c6174865c06efc04b0f79034ef44c63869c3
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-25 15:23:44 +0000

    Implement Product/Validations name attribute presence test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+
+    #all attributes is present
     it "is valid" do
       @product = Product.new
       @category = Category.new
@@ -11,6 +13,18 @@ RSpec.describe Product, type: :model do
       @product.quantity = 13
       @product.category = @category
       expect(@product.valid?).to be true
+    end
+
+    #name attribute
+    it "name attribute presence" do
+      @product = Product.new
+      @product.name = nil #invalid
+      @product.valid?
+      expect(@product.errors[:name]).to include("can't be blank")
+  
+      @product.name = 'Product Test' #valid
+      @product.valid? 
+      expect(@product.errors[:name]).not_to include("can't be blank")
     end
   end
 end

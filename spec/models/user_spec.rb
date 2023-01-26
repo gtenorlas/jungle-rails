8b9cab127fe41c5663c8bf1763bc1341d16ebd35
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-26 13:48:05 +0000

    Implement user validation all valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it "is valid" do
+      user = User.new(
+        first_name: 'abc',
+        last_name: 'd',
+        email: 'email@test.com',
+        password: '12345678',
+        password_confirmation: '12345678'
+      )
+      expect(user).to be_valid
+    end
+
+    
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
 end

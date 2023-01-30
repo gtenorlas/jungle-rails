5ec741491eae2f973a57dbffc658cf1a28ed6cf4
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-30 14:01:18 +0000

    Implement test homepage/2 products on page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -7,4 +7,8 @@ describe('Homepage', () => {
     cy.visit('http://localhost:3000')
     cy.get(".products article").should("be.visible");
   });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 })
